8c860e1c199efb7f8705ee791f486a6933f887e2
Yusuke <yhakamaya@adobe.com>
2022-08-11 22:20:59 +0900

    Configure Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:motchi/firebase_options.dart';
 
 import 'pages/home_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Motchi());
 }
 
